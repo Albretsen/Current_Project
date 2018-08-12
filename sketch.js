b3c65b36bd45a94b1fbdc5b30189d086a2c4ec13
Aski09 <bjellanda@gmail.com>
2018-08-12 12:06:16 +0200

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,97 @@
+var cols = 10;
+var rows = 10;
+
+var y = 0;
+var x = 0;
+var y1 = 0;
+var x1 = 0;
+
+var grid;
+
+var currPos;
+
+function setup() {
+  createCanvas(600, 300);
+
+  grid = create2DArray();
+  
+  for(var i = 0; i < cols; i++){
+    for(var j = 0; j < rows; j++){
+      grid[i][j] = 0;
+    }
+  }
+  grid[0][3] = 1;
+}
+
+function create2DArray() {
+  var arr = new Array(cols);
+
+  for (var i = 0; i < arr.length; i++) {
+    arr[i] = new Array(rows);
+  }
+
+  return arr;
+}
+
+function drawEachSquare(){
+  fill(255);
+  stroke(0);
+  
+  x = 0;
+  for(var i = 0; i < cols; i++){
+    for(var j = 0; j < rows; j++){
+      if(grid[i][j] != 0){
+        fill(0);
+      } else{
+        fill(255);
+      }
+      rect(x,y,10,10);
+      y += 10;
+    }
+    y = 0;
+    x += 10;
+  }
+}
+
+function findCurrPos(){
+  
+  for(var i = 0; i < cols; i++){
+    for(var j = 0; j < rows; j++){
+      if(grid[i][j] == 1){
+        return[i,j];
+      }
+    }
+  }
+  return[0,0];
+  
+}
+
+function move(x1,y1){
+  currPos = findCurrPos();
+  
+  grid[currPos[0]][currPos[1]] = 0;
+  
+  currPos[0] = currPos[0] + x1;
+  currPos[1] = currPos[1] + y1;
+  
+  grid[currPos[0]][currPos[1]] = 1;
+  
+}
+
+function keyPressed(){
+  if(keyCode == LEFT_ARROW){
+    move(-1,0);
+  } else if(keyCode == RIGHT_ARROW){
+    move(1,0);
+  } else if(keyCode == DOWN_ARROW){
+    move(0,1);;
+  } else if(keyCode == UP_ARROW){
+    move(0,-1);
+  }
+}
+
+function draw() {
+  background(0);
+  
+  drawEachSquare();
+}
